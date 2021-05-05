7a4c70497a4e69222970fd46423416bb1421cf9c
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-05-04 18:03:24 -0700

    Add archive event page

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -27,7 +27,7 @@ function university_adjust_queries($query)
 {
   if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
     $today = date('Ymd');
-    $query->set('meta_key', 'event_date');
+    $query->set('metakey', 'event_date');
     $query->set('orderby', 'meta_value_num');
     $query->set('order', 'ASC');
     $query->set('meta_query', array(
